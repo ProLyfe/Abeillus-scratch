7b1160bf0d1de1b72a86bacae10a7de09025a676
ProLyfe <Rayane.prolyfe@gmail.com>
2020-06-04 14:25:24 +0200

    add contact form

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const nom = form.firstname.value;
+    const prenom = form.lastname.value;
+    const mail = form.mail.value;
+    const sujet = form.subject.value;
+
+    const client = {
+        nom: nom,
+        prenom: prenom,
+        mail: mail,
+        sujet: sujet
+    };
+
+    db.collection('formulaire').add(client).then(() => {
+        console.log('Envoi réussi');
+    }).catch(err => console.log(err))
+    
+    // console.log(client)
+})
+
+// console.log(db.collection('formulaire'))
